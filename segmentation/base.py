f12b1eb8dfb769e4b28bc7d64a10f33f448bac12
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-18 13:38:24 +0800

    update

--- segmentation/base.py
+++ segmentation/base.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
+from torch.cuda.amp import autocast
 
-__all__ = ['NeuralNetwork']
+__all__ = ['NeuralNetwork', 'SegmentationNetwork']
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
@@ -46,3 +47,83 @@ class SegmentationNetwork(NeuralNetwork):
         # can be expensive, so it makes sense to save and reuse them.
         self._gaussian_3d = self._patch_size_for_gaussian_3d = None
         self._gaussian_2d = self._patch_size_for_gaussian_2d = None
+    
+    def predict_2D(self, x, do_mirroring: bool, mirror_axes: tuple = (0, 1, 2), use_sliding_window: bool = False,
+                   step_size: float = 0.5, patch_size: tuple = None, regions_class_order: tuple = None,
+                   use_gaussian: bool = False, pad_border_mode: str = "constant",
+                   pad_kwargs: dict = None, all_in_gpu: bool = False,
+                   verbose: bool = True, mixed_precision: bool = True):
+        """
+        Use this function to predict a 2D image. If this is a 3D U-Net it will crash because you cannot predict a 2D
+        image with that (you dummy).
+
+        When running predictions, you need to specify whether you want to run fully convolutional of sliding window
+        based inference. We very strongly recommend you use sliding window with the default settings.
+
+        It is the responsibility of the user to make sure the network is in the proper mode (eval for inference!). If
+        the network is not in eval mode it will print a warning.
+
+        :param x: Your input data. Must be a nd.ndarray of shape (c, x, y).
+        :param do_mirroring: If True, use test time data augmentation in the form of mirroring
+        :param mirror_axes: Determines which axes to use for mirroing. Per default, mirroring is done along all three
+        axes
+        :param use_sliding_window: if True, run sliding window prediction. Heavily recommended! This is also the default
+        :param step_size: When running sliding window prediction, the step size determines the distance between adjacent
+        predictions. The smaller the step size, the denser the predictions (and the longer it takes!). Step size is given
+        as a fraction of the patch_size. 0.5 is the default and means that wen advance by patch_size * 0.5 between
+        predictions. step_size cannot be larger than 1!
+        :param patch_size: The patch size that was used for training the network. Do not use different patch sizes here,
+        this will either crash or give potentially less accurate segmentations
+        :param regions_class_order: Fabian only
+        :param use_gaussian: (Only applies to sliding window prediction) If True, uses a Gaussian importance weighting
+         to weigh predictions closer to the center of the current patch higher than those at the borders. The reason
+         behind this is that the segmentation accuracy decreases towards the borders. Default (and recommended): True
+        :param pad_border_mode: leave this alone
+        :param pad_kwargs: leave this alone
+        :param all_in_gpu: experimental. You probably want to leave this as is it
+        :param verbose: Do you want a wall of text? If yes then set this to True
+        :return:
+        """
+        torch.cuda.empty_cache()
+
+        assert step_size <= 1, 'step_size must be smaler than 1. Otherwise there will be a gap between consecutive ' \
+                               'predictions'
+
+        if self.conv_op == nn.Conv3d:
+            raise RuntimeError("Cannot predict 2d if the network is 3d. Dummy.")
+
+        if verbose: print("debug: mirroring", do_mirroring, "mirror_axes", mirror_axes)
+
+        if pad_kwargs is None:
+            pad_kwargs = {'constant_values': 0}
+
+        # A very long time ago the mirror axes were (2, 3) for a 2d network. This is just to intercept any old
+        # code that uses this convention
+        if len(mirror_axes):
+            if max(mirror_axes) > 1:
+                raise ValueError("mirror axes. duh")
+
+        if self.training:
+            print('WARNING! Network is in train mode during inference. This may be intended, or not...')
+
+        assert len(x.shape) == 3, "data must have shape (c,x,y)"
+
+        if mixed_precision:
+            context = autocast
+        else:
+            context = no_op
+
+        with context():
+            with torch.no_grad():
+                if self.conv_op == nn.Conv2d:
+                    if use_sliding_window:
+                        res = self._internal_predict_2D_2Dconv_tiled(x, step_size, do_mirroring, mirror_axes, patch_size,
+                                                                     regions_class_order, use_gaussian, pad_border_mode,
+                                                                     pad_kwargs, all_in_gpu, verbose)
+                    else:
+                        res = self._internal_predict_2D_2Dconv(x, patch_size, do_mirroring, mirror_axes, regions_class_order,
+                                                               pad_border_mode, pad_kwargs, verbose)
+                else:
+                    raise RuntimeError("Invalid conv op, cannot determine what dimensionality (2d/3d) the network is")
+
+        return res
