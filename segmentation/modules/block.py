e111672a47898acfb665b9506e6243d2c3f0241d
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-18 22:18:18 +0800

    update

--- segmentation/modules/block.py
+++ segmentation/modules/block.py
@@ -4,7 +4,11 @@ import torch.nn as nn
 __all__ = ['ConvDropNorm']
 
 class ConvDropNorm(nn.Module):
-    def __init__(self):
+    def __init__(self, input_channels, output_channels, 
+                 conv_op=nn.Conv2d, conv_kwargs=None,
+                 norm_op=nn.BatchNorm2d, norm_op_kwargs=None,
+                 dropout_op =nn.Dropout2d, dropout_op_kwargs=None,
+                 nonlin=nn.LeakyReLU, nonlin_kwargs=None):
         super(ConvDropNorm).__init__()
     
     def forward(self, x):
