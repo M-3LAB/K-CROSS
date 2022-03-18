e86f37d4f7fb9e60ea18b6a25983b205b3f710ed
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-18 22:52:18 +0800

    update

--- segmentation/modules/block.py
+++ segmentation/modules/block.py
@@ -19,6 +19,27 @@ class ConvDropNorm(nn.Module):
             norm_op_kwargs = {'eps': 1e-5, 'affine': True, 'momentum': 0.1}
         if conv_kwargs is None:
             conv_kwargs = {'kernel_size': 3, 'stride': 1, 'padding': 1, 'dilation': 1, 'bias': True}
+        
+        self.nonlin_kwargs = nonlin_kwargs
+        self.nonlin = nonlin
+        self.dropout_op = dropout_op
+        self.dropout_op_kwargs = dropout_op_kwargs
+        self.norm_op_kwargs = norm_op_kwargs
+        self.conv_kwargs = conv_kwargs
+        self.conv_op = conv_op
+        self.norm_op = norm_op
+
+        self.conv = self.conv_op(input_channels, output_channels, **self.conv_kwargs)
+        if self.dropout_op is not None and self.dropout_op_kwargs['p'] is not None and self.dropout_op_kwargs[
+            'p'] > 0:
+            self.dropout = self.dropout_op(**self.dropout_op_kwargs)
+        else:
+            self.dropout = None
+        self.instnorm = self.norm_op(output_channels, **self.norm_op_kwargs)
+        self.lrelu = self.nonlin(**self.nonlin_kwargs)
     
     def forward(self, x):
-        pass
+        x = self.conv(x)
+        if self.dropout is not None:
+            x = self.dropout(x)
+        return self.lrelu(self.instnorm(x))
