e06b6196a0ccb68ba0edbc94ffa33385e4e660a4
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-18 21:46:16 +0800

    update

---
+++ segmentation/modules/block.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+__all__ = ['ConvDropNorm']
+
+class ConvDropNorm(nn.Module):
+    def __init__(self):
+        super(ConvDropNorm).__init__()
+    
+    def forward(self, x):
+        pass
