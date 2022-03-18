c80eb5c2f8cca1962ca38325eea3986107734ced
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-18 22:25:03 +0800

    update

--- segmentation/modules/block.py
+++ segmentation/modules/block.py
@@ -10,6 +10,15 @@ class ConvDropNorm(nn.Module):
                  dropout_op =nn.Dropout2d, dropout_op_kwargs=None,
                  nonlin=nn.LeakyReLU, nonlin_kwargs=None):
         super(ConvDropNorm).__init__()
+
+        if nonlin_kwargs is None:
+            nonlin_kwargs = {'negative_slope': 1e-2, 'inplace': True}
+        if dropout_op_kwargs is None:
+            dropout_op_kwargs = {'p': 0.5, 'inplace': True}
+        if norm_op_kwargs is None:
+            norm_op_kwargs = {'eps': 1e-5, 'affine': True, 'momentum': 0.1}
+        if conv_kwargs is None:
+            conv_kwargs = {'kernel_size': 3, 'stride': 1, 'padding': 1, 'dilation': 1, 'bias': True}
     
     def forward(self, x):
         pass
