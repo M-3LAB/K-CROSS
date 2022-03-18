ec43b5ec7d2bdd35f082be42845affc761b7b444
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-18 10:44:15 +0800

    update

---
+++ segmentation/base.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+
+    def get_device(self):
+        if next(self.parameters()).device.type == "cpu":
+            return "cpu"
+        else:
+            return next(self.parameters()).device.index
+
+    def set_device(self, device):
+        if device == "cpu":
+            self.cpu()
+        else:
+            self.cuda(device)
+
+    def forward(self, x):
+        raise NotImplementedError
