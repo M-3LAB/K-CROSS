57f0011bff5a4a0fab386460befb3a31379a4399
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-18 15:27:36 +0800

    update

--- segmentation/common.py
+++ segmentation/common.py
@@ -1,5 +1,6 @@
+import torch
 
-__all__ = ['no_op']
+__all__ = ['no_op', 'maybe_to_torch']
 
 class no_op(object):
     def __enter__(self):
@@ -7,4 +8,19 @@ class no_op(object):
 
     def __exit__(self, *args):
         pass
+
+def maybe_to_torch(data):
+    if isinstance(data, list):
+        data = [maybe_to_torch(i) if not isinstance(i, torch.Tensor) else i for i in data]
+    elif not isinstance(data, torch.Tensor):
+        data = torch.from_numpy(data).float()
+    return data
+
+
+def to_cuda(data, non_blocking=True, gpu_id=0):
+    if isinstance(data, list):
+        data = [i.cuda(gpu_id, non_blocking=non_blocking) for i in data]
+    else:
+        data = data.cuda(gpu_id, non_blocking=non_blocking)
+    return data
 
