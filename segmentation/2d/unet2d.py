ab4b36b14a8cf85c1d4691f38aa6dda3a1608e3b
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-19 10:04:43 +0800

    update

--- segmentation/2d/unet2d.py
+++ segmentation/2d/unet2d.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 from segmentation.base import SegmentationNetwork
+from segmentation.modules.block import ConvDropNorm
+from segmentation.modules.stackedconv import StackedConvLayers
 
 class UNet2D(SegmentationNetwork):
     def __init__(self):
