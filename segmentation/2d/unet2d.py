0befca59e2e20882d06fa9976be1ed033052052a
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-19 10:44:33 +0800

    update

--- segmentation/2d/unet2d.py
+++ segmentation/2d/unet2d.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from segmentation.base import SegmentationNetwork
 from segmentation.modules.block import ConvDropNorm
 from segmentation.modules.stackedconv import StackedConvLayers
+from segmentation.common import softmax_helper, InitWeights_He
 
 class UNet2D(SegmentationNetwork):
     def __init__(self, input_channels, base_num_features, num_classes, num_pool, num_conv_per_stage=2,
