96e536608320fde1215b37a30861ec8b8e9384d4
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-20 10:07:09 +0800

    update

---
+++ loss_function/segment/cross_entropy.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+__all__ = ['RobustCE']
+
+class RobustCE(nn.CrossEntropyLoss):
+    """
+    this is just a compatibility layer because my target tensor is float and has an extra dimension
+    """
+    def forward(self, input: Tensor, target: Tensor):
+        if len(target.shape) == len(input.shape):
+            assert target.shape[1] == 1
+            target = target[:, 0]
+        return super().forward(input, target.long())
