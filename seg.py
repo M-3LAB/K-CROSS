0d5028696f27aa0c2e15b1e0a9e7841faf340474
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-20 15:54:04 +0800

    update

--- seg.py
+++ seg.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from architecture.unet.unet2d import Unet2DTrainer
+from configuration.seg.config import parse_arguments_seg
 
 if __name__ == 'main':
-    pass
+    args = parse_arguments_seg()
