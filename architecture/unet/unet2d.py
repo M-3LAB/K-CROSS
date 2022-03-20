fe526b56d91baff6ddcf0380c928399e1b0a971e
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-20 16:15:26 +0800

    update

--- architecture/unet/unet2d.py
+++ architecture/unet/unet2d.py
@@ -2,6 +2,7 @@ from architecture.centralized.train import CentralizedTrain
 import torch
 import torch.nn as nn
 from architecture.centralized.train import CentralizedTrain
+import yaml
 
 __all__ = ['Unet2DTrainer']
 
@@ -9,3 +10,8 @@ class Unet2DTrainer(CentralizedTrain):
     def __init__(self, args):
         super(Unet2DTrainer, self).__init__(args=args)
         self.args = args
+    
+    def load_config(self):
+       # load dataset 
+       with open('./configuration/seg/{}.yaml'.format(self.args.dataset), 'r') as f:
+            config_seg = yaml.load(f, Loader=yaml.SafeLoader)
