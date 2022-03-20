7b345855dfbf3044daa832cf9b5c2981a08396a8
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-20 16:29:08 +0800

    update

--- architecture/unet/unet2d.py
+++ architecture/unet/unet2d.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 from architecture.centralized.train import CentralizedTrain
 import yaml
+from tools.utilize import *
 
 __all__ = ['Unet2DTrainer']
 
@@ -14,5 +15,18 @@ class Unet2DTrainer(CentralizedTrain):
     
     def load_config(self):
        # load dataset 
-       with open('./configuration/seg/{}.yaml'.format(self.args.dataset), 'r') as f:
+        with open('./configuration/seg/{}.yaml'.format(self.args.dataset), 'r') as f:
             config_seg = yaml.load(f, Loader=yaml.SafeLoader)
+        # load basic 
+        with open('./configuration/architecture/3_dataset_base/{}.yaml'.format(self.args.dataset), 'r') as f:
+            config_dataset = yaml.load(f, Loader=yaml.SafeLoader)
+        with open('configuration/architecture/2_train_base/centralized_training.yaml', 'r') as f:
+            config_train = yaml.load(f, Loader=yaml.SafeLoader)
+        with open('configuration/architecture/1_model_base/{}.yaml'.format(self.args.model), 'r') as f:
+            config_model = yaml.load(f, Loader=yaml.SafeLoader)
+        
+        config = override_config(config_model, config_train)
+        config = override_config(config, config_dataset)
+        config = override_config(config, config_seg)
+        self.para_dict = merge_config(config, self.args)
+        self.args = extract_config(self.args)
