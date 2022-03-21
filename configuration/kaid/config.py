ba60059de30d9f005ae94b4bfad34787b1536685
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-21 11:09:06 +0800

    update

--- configuration/kaid/config.py
+++ configuration/kaid/config.py
@@ -33,6 +33,9 @@ def parse_arguments_kaid():
     parser.add_argument('--beta1', type=float, default=None, help='Adam Optimizer parameter')
     parser.add_argument('--beta2', type=float, default=None, help='Adam Optimizer parameter')
     parser.add_argument('--fid', action='store_true', default=True)
+    parser.add_argument('--resume', action='store_true', default=None, help='resume training or not')
+    parser.add_argument('--train', action='store_true', default=None)
+    parser.add_argument('--validate', action='store_true', default=None)
     #segmentation
     parser.add_argument('--segment', action='store_true', default=None, help='segmentation method evaluation or not')
     parser.add_argument('--seg-method', type=str, choices=['2d', '3d'])
