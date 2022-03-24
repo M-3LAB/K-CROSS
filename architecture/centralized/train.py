17357b396d4ffdc22bb3f8533582f7c5d6fa3d09
jinbao-wang <linkingring@163.com>
2022-03-25 01:47:25 +0800

    update

--- architecture/centralized/train.py
+++ architecture/centralized/train.py
@@ -18,18 +18,26 @@ class CentralizedTrain():
         self.args = args
 
     def load_config(self):
+        # load basic 
+        config_base = self.load_basic_config()
+        # obtain the final configuratuation, para_dict
+        self.para_dict = merge_config(config_base, self.args)
+        # local args
+        self.args = extract_config(self.args)
+
+    def load_basic_config(self):
+        # load predefined parameter
         with open('./configuration/architecture/3_dataset_base/{}.yaml'.format(self.args.dataset), 'r') as f:
             config_dataset = yaml.load(f, Loader=yaml.SafeLoader)
-        with open('./configuration/architecture/2_train_base/centralized_training.yaml', 'r') as f:
+        with open('configuration/architecture/2_train_base/centralized_training.yaml', 'r') as f:
             config_train = yaml.load(f, Loader=yaml.SafeLoader)
-        with open('./configuration/architecture/1_model_base/{}.yaml'.format(self.args.model), 'r') as f:
+        with open('configuration/architecture/1_model_base/{}.yaml'.format(self.args.model), 'r') as f:
             config_model = yaml.load(f, Loader=yaml.SafeLoader)
 
         config = override_config(config_model, config_train)
         config = override_config(config, config_dataset)
-        self.para_dict = merge_config(config, self.args)
-        self.args = extract_config(self.args)
 
+        return config
 
     def preliminary(self):
         print('---------------------')
