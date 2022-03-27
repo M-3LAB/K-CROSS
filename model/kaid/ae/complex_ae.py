32e695448f02c3a21a7e6177f6709c6af5547ca3
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-27 16:10:50 +0800

    complex ae starts

--- model/kaid/ae/complex_ae.py
+++ model/kaid/ae/complex_ae.py
@@ -1,7 +1,13 @@
+from turtle import forward
 import torch
 import torch.nn as nn
 
 __all__ = ['ComplexUnet']
 
-
+class ComplexUnet(nn.Module):
+    def __init__(self):
+        super(ComplexUnet).__init__()
+    
+    def forward(self, x):
+        pass
 
