fbc1ee6e7be272aaad06585890e87f84f5c2c5e8
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-28 10:47:01 +0800

    add complexConvTranspose2d

--- model/kaid/complex_nn/fourier_convolve.py
+++ model/kaid/complex_nn/fourier_convolve.py
@@ -20,9 +20,25 @@ class ComplexConv2d(nn.Module):
         return output
 
 class ComplexConvTranspose2d(nn.Module):
-    def __init__(self):
+    def __init__(self, inc, ouc, ks, stride=1, padding=0, dilation=1, groups=1, bias=True, 
+                 output_padding=0, padding_modes='zeros'):
         super(ComplexConvTranspose2d, self).__init__()
+
+        # Real part convolution
+        self.conv_re = nn.ConvTranspose2d(inc, ouc, ks, stride=stride, padding=padding, 
+                                          output_padding=output_padding, groups=groups, bias=bias,
+                                          dilation=dilation, padding_modes=padding_modes)
+
+        # Imaginary part convolution
+        self.conv_im = nn.ConvTranspose2d(inc, ouc, ks, stride=stride, padding=padding, 
+                                          output_padding=output_padding, groups=groups, bias=bias,
+                                          dilation=dilation, padding_modes=padding_modes)
+
+        
     
     def forward(self, x):
-        pass
+        out_real = self.conv_re(x.real) - self.conv_im(x.imag)
+        out_imginary = self.conv_im(x.imag) + self.conv_im(x.imag)
+        output = torch.complex(out_real, out_imginary)
+        return output        
 
