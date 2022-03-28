34c7f0f7fc907f35dddcc8f6e9d3ae3841e6aa73
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-28 21:32:36 +0800

    update

--- model/kaid/ae/modules/encoder.py
+++ model/kaid/ae/modules/encoder.py
@@ -6,7 +6,7 @@ from model.kaid.complex_nn.op import *
 __all__ = ['ComplexEncoder', 'ComplexUnetDown']
 
 class ComplexUnetDown(nn.Module):
-    def __init__(self, inc, ouc, ks, stride=2, padding=1, slope=0.2):
+    def __init__(self, inc, ouc, ks=3, stride=2, padding=1, slope=0.2):
         super(ComplexUnetDown, self).__init__()
         self.inc = inc
         self.ouc = ouc
@@ -26,9 +26,22 @@ class ComplexUnetDown(nn.Module):
         output = self.model(x)
         return output
 class ComplexEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, ouc_list=[64, 128, 256, 512, 512]):
         super(ComplexEncoder).__init__()
+        self.ouc_list = ouc_list
+        self.down1 = ComplexUnetDown(1, 64)
+        self.down2 = ComplexUnetDown(64, 128)
+        self.down3 = ComplexUnetDown(128, 256)
+        self.down4 = ComplexUnetDown(256, 512)
+        self.down5 = ComplexUnetDown(512, 512)
 
     def forward(self, x):
-        pass
+        self.d1 = self.down1(x)
+        self.d2 = self.down2(self.d1)
+        self.d3 = self.down3(self.d2)
+        self.d4 = self.down4(self.d3)
+        z = self.down5(self.d4)
+        return z
+        
+        
 
