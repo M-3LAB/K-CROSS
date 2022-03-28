3bcb1e823d3acc381310d50dd6f6cf5af134850e
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-28 15:27:22 +0800

    update

--- model/kaid/ae/modules/encoder.py
+++ model/kaid/ae/modules/encoder.py
@@ -3,6 +3,12 @@ import torch.nn as nn
 
 __all__ = ['ComplexEncoder']
 
+class ComplexUnetUp(nn.Module):
+    def __init__(self):
+        super(ComplexUnetUp, self).__init__()
+
+    def forward(self, x):
+        pass
 class ComplexEncoder(nn.Module):
     def __init__(self):
         super(ComplexEncoder).__init__()
