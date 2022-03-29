4bfec512c21149e4d01e85565e1f309cd02261d7
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-29 15:35:46 +0800

    complex upsample added

--- model/kaid/complex_nn/op.py
+++ model/kaid/complex_nn/op.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-__all__ = ['NaiveComplexBatchNorm2d', 'ComplexRELU', 'ComplexLeakyRELU']
+__all__ = ['NaiveComplexBatchNorm2d', 'ComplexRELU', 'ComplexLeakyRELU', 'ComplexUpsample']
 class NaiveComplexBatchNorm2d(nn.Module):
     def __init__(self, num_features, eps=1e-5, momentum=0.1, affine=True,
                  track_running_stats=True):
@@ -46,6 +46,19 @@ class ComplexTanh(nn.Module):
         super(ComplexTanh, self).__init__()
         self.act = nn.Tanh()
     
+    def forward(self, x):
+        output_real = self.act(x.real)
+        output_imginary = self.act(x.imag)
+        output = torch.complex(output_real, output_imginary)
+        return output
+
+class ComplexUpsample(nn.Module):
+    def __init__(self, scale_factor=2):
+        super(ComplexUpsample, self).__init__()
+
+        self.scale_factor = scale_factor
+        self.act = nn.Upsample(scale_factor=self.scale_factor)
+
     def forward(self, x):
         output_real = self.act(x.real)
         output_imginary = self.act(x.imag)
