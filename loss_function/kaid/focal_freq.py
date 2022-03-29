a1ccd62be5b7ca8dd3d1f7cc5768ab6ba92a7ba9
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-29 16:00:04 +0800

    update

--- loss_function/kaid/focal_freq.py
+++ loss_function/kaid/focal_freq.py
@@ -1,2 +1,11 @@
 import torch
 import torch.nn as nn
+
+__all__ = ['FocalFreqLoss']
+
+class FocalFreqLoss(nn.Module):
+    def __init__(self):
+        super(FocalFreqLoss, self).__init__()
+    
+    def forward(self, x):
+        pass
