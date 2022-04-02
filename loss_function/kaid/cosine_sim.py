312a46a076b59ff516a7e742fcd1d48ebcefe93c
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-02 22:51:35 +0800

    cosine sim loss finished

--- loss_function/kaid/cosine_sim.py
+++ loss_function/kaid/cosine_sim.py
@@ -4,4 +4,6 @@ import torch.nn as nn
 __all__ = ['cosine_sim_loss']
 
 def cosine_sim_loss(real_z, noise_z):
-    pass
+    cos = nn.CosineSimilarity(dim=-2, eps=1e-6)
+    distance = cos(real_z, noise_z)
+    return torch.mean(distance)
