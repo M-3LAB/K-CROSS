33902e9b094501b6767ae8691bb9a8dbd8dbeeb9
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-02 15:05:47 +0800

    update

--- configuration/kaid/config.py
+++ configuration/kaid/config.py
@@ -30,6 +30,8 @@ def parse_arguments_kaid():
     parser.add_argument('--fid', action='store_true', default=True)
     parser.add_argument('--train', action='store_true', default=None)
     parser.add_argument('--validate', action='store_true', default=None)
+    parser.add_argument('--noisy-loss', action='store_true', default=None)
+
     #segmentation
     parser.add_argument('--segment', action='store_true', default=None, help='segmentation method evaluation or not')
     parser.add_argument('--seg-method', type=str, choices=['2d', '3d'])
