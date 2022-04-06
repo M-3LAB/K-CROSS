7312155b27012b18b5579784243091f4147cc38f
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-07 07:36:06 +0800

    update noisy loss

--- loss_function/kaid/cosine_sim.py
+++ loss_function/kaid/cosine_sim.py
@@ -6,11 +6,11 @@ __all__ = ['cosine_sim_loss', 'complex_cosine_sim_loss']
 def cosine_sim_loss(real_z, noise_z):
     cos = nn.CosineSimilarity(dim=-2, eps=1e-6)
     distance = cos(real_z, noise_z)
-    return torch.mean(distance)
+    return 1-torch.mean(distance)
 
 def complex_cosine_sim_loss(real_freq_z, noise_freq_z):
     real_freq_z = torch.stack([real_freq_z.real, real_freq_z.imag], dim=-1)
     noise_freq_z = torch.stack([noise_freq_z.real, noise_freq_z.imag], dim=-1)
     cos = nn.CosineSimilarity(dim=-3, eps=1e-6)
     distance = cos(real_freq_z, noise_freq_z)
-    return torch.mean(distance)
+    return 1-torch.mean(distance)
