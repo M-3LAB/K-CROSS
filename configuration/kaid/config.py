fd93756f853a6cae13eba58165a71f95199d22dd
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-04-08 13:19:41 +0800

    add one_z and all_z flag

--- configuration/kaid/config.py
+++ configuration/kaid/config.py
@@ -35,6 +35,7 @@ def parse_arguments_kaid():
     parser.add_argument('--nirps-path', type=str, default='./nirps_dataset', help='nirps data path')
     parser.add_argument('--test-model', type=str, default=None, choices=['cyclegan','munit','unit'])
     parser.add_argument('--infer-range', type=str, default=None, choices=['all', 'ixi', 'brats2021'])
+    parser.add_argument('--latent-size', type=str, default=None, choices=['one_z', 'all_z'])
 
     args = parser.parse_args()
     return args
